bc40ea930738d36ea277f2fe534d28ebe17148c1
misunghq <misunghq@misung.biz>
2014-09-26 10:09:19 +0000

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Msp::Application.config.secret_key_base = '4fc9b5e557fbdb4dd6368644edda11f0b423e82eb4d54d1dd9b8254bb44fd4cd3866482a08e529ec9c7ef66ef8f3f392665c6b24e9531b7fbbb9b7a712291ca3'
